08baecf2e210ec2c6615c5f3b0b25ff0a67e35cb
Evo <evo@isecc.com>
2020-01-19 08:49:44 -0500

    e

--- src/main/scala/RandomDSSplitter.scala
+++ src/main/scala/RandomDSSplitter.scala
@@ -9,86 +9,89 @@ import org.apache.spark.sql.functions._
 
 object RandomDSSplitter {
 
-  //########################## Get the Parameters of the Job ##########################################
-  // Assuming that application.properties is in the root folder of the spark job
+  def main(args: Array[String]): Unit = {
 
-  val properties: Properties = new Properties()
+    //########################## Get the Parameters of the Job ##########################################
+    // Assuming that application.properties is in the root folder of the spark job
 
-  if (Files.exists(Paths.get("./dssplitter.properties"))) {
-    val source = Source.fromURL("file:./dssplitter.properties")
-    properties.load(source.bufferedReader())
+    val properties: Properties = new Properties()
+
+    if (Files.exists(Paths.get("./dssplitter.properties"))) {
+      val source = Source.fromURL("file:./dssplitter.properties")
+      properties.load(source.bufferedReader())
+    }
+    else if (Files.exists(Paths.get("dssplitter.properties"))) {
+      val source = Source.fromURL("file:dssplitter.properties")
+      properties.load(source.bufferedReader())
+    }
+    else {
+      println("no properties file, exiting")
+      println(System.getProperty("user.dir"))
+      System.exit(0)
+    }
+
+    val master = properties.getProperty("master")
+    println(master)
+    val fileName = properties.getProperty("input.file.name")
+    println(fileName)
+    val outFileName = properties.getProperty("output.file.name")
+    println(outFileName)
+    val hiveSupport = properties.getProperty("hive.support").toBoolean
+    println(hiveSupport)
+    val parThreads = properties.getProperty("spark.parallel.threads").toInt
+    println(parThreads)
+    val fileFormat = properties.getProperty("format")
+    println(fileFormat)
+    val partitioned = properties.getProperty("partitioned").toBoolean
+    println(partitioned)
+    val partitionName = properties.getProperty("partition.field.name")
+    println(partitionName)
+
+
+    val sparkT = SparkSession.builder
+      .master(master)
+      .appName("random-ds-splitter")
+      //put here any required param controlling Vectorization (see all available params listed at the beginning)
+      .config("spark.sql.parquet.enableVectorizedReader ", "true")
+
+
+    if (hiveSupport) {
+
+      sparkT.config("spark.sql.warehouse.dir", "/opt/dwh")
+      sparkT.enableHiveSupport()
+
+    }
+
+    val spark = sparkT.getOrCreate()
+
+    var mainpartDF: DataFrame = null
+    var mainpartDF2: DataFrame = null
+
+    if (fileFormat.equalsIgnoreCase("csv")) {
+
+      mainpartDF = spark.read.format("csv")
+        .option("sep", ",")
+        .option("inferSchema", "true")
+        .option("header", "true")
+        .load(fileName)
+        .repartition(parThreads)
+
+    }
+
+    if (fileFormat.equalsIgnoreCase("parquet")) {
+
+      mainpartDF = spark.read.format("parquet")
+        //.option("sep", ",")
+        //.option("inferSchema", "true")
+        .option("header", "true")
+        .load(fileName)
+        .repartition(parThreads)
+
+    }
+
+    mainpartDF.printSchema()
+    mainpartDF.show()
+
   }
-  else if (Files.exists(Paths.get("dssplitter.properties"))) {
-    val source = Source.fromURL("file:dssplitter.properties")
-    properties.load(source.bufferedReader())
-  }
-  else {
-    println("no properties file, exiting")
-    println(System.getProperty("user.dir"))
-    System.exit(0)
-  }
-
-  val master = properties.getProperty("master")
-  println(master)
-  val fileName = properties.getProperty("input.file.name")
-  println(fileName)
-  val outFileName = properties.getProperty("output.file.name")
-  println(outFileName)
-  val hiveSupport = properties.getProperty("hive.support").toBoolean
-  println(hiveSupport)
-  val parThreads = properties.getProperty("spark.parallel.threads").toInt
-  println(parThreads)
-  val fileFormat = properties.getProperty("format")
-  println(fileFormat)
-  val partitioned = properties.getProperty("partitioned").toBoolean
-  println(partitioned)
-  val partitionName = properties.getProperty("partition.field.name")
-  println(partitionName)
-
-
-  val sparkT = SparkSession.builder
-    .master(master)
-    .appName("random-ds-splitter")
-    //put here any required param controlling Vectorization (see all available params listed at the beginning)
-    .config("spark.sql.parquet.enableVectorizedReader ", "true")
-
-
-  if (hiveSupport) {
-
-    sparkT.config("spark.sql.warehouse.dir", "/opt/dwh")
-    sparkT.enableHiveSupport()
-
-  }
-
-  val spark = sparkT.getOrCreate()
-
-  var mainpartDF : DataFrame = null
-  var mainpartDF2 : DataFrame = null
-
-  if (fileFormat.equalsIgnoreCase("csv")) {
-
-    mainpartDF = spark.read.format("csv")
-      .option("sep", ",")
-      .option("inferSchema", "true")
-      .option("header", "true")
-      .load(fileName)
-      .repartition(parThreads)
-
-  }
-
-  if (fileFormat.equalsIgnoreCase("parquet")) {
-
-    mainpartDF = spark.read.format("parquet")
-      //.option("sep", ",")
-      //.option("inferSchema", "true")
-      .option("header", "true")
-      .load(fileName)
-      .repartition(parThreads)
-
-  }
-
-  mainpartDF.printSchema()
-  mainpartDF.show()
-
 
 }
