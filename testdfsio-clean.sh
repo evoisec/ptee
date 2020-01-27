5742a74e4edeec3a59eeab916d74a57317754eae
Evo <evo@isecc.com>
2020-01-27 07:28:38 -0500

    e

---
+++ testdfsio-clean.sh
@@ -0,0 +1,2 @@
+hadoop jar /opt/cloudera/parcels/CDH-5.16.1-1.cdh5.16.1.p0.3/lib/hadoop-mapreduce/hadoop-mapreduce-client-jobclient-tests.jar \
+TestDFSIO -Dtest.build.data=/perf_testing/synthetic1 -clean
